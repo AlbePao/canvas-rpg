e0d7a372a4c6256ae31f918b81c0ecfa054698fd
Alberto Paolucci <apaolucci@softstrategy.it>
2026-05-07 13:07:51 +0200

    add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,83 @@
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    ignores: ['dist/**', '**/*.html', 'eslint.config.mjs'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      eslintPluginPrettierRecommended,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        allowDefaultProject: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'memberLike',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
+        },
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'variable',
+          modifiers: ['const'],
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        },
+      ],
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/no-empty-function': [
+        'error',
+        {
+          allow: ['arrowFunctions'],
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'none',
+        },
+      ],
+      '@typescript-eslint/unbound-method': [
+        'error',
+        {
+          ignoreStatic: true,
+        },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: true,
+        },
+      ],
+      'no-unused-expressions': 'error',
+      'sort-imports': 'off',
+      'arrow-body-style': 'error',
+      eqeqeq: 'error',
+      curly: 'error',
+      'no-nested-ternary': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [eslintPluginPrettierRecommended],
+  },
+]);
