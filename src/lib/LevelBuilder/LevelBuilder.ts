09d73e82dc26bc76a1541d84b8a6a2fc625b8a3d
Alberto Paolucci <apaolucci@softstrategy.it>
2026-05-12 17:50:29 +0200

    level builder from typescript files

--- src/lib/LevelBuilder/LevelBuilder.ts
+++ src/lib/LevelBuilder/LevelBuilder.ts
@@ -1,27 +1,40 @@
 import { CHANGE_LEVEL, HERO_EXITS } from '../../constants/events';
 import { gridCells } from '../../helpers/grid';
 import { objectKeys } from '../../helpers/objectKeys';
-import { Exit, ExitData } from '../../objects/Exit';
+import type { ExitData } from '../../objects/Exit';
+import { Exit } from '../../objects/Exit';
 import { Hero } from '../../objects/Hero';
 import { CollectibleItem } from '../../objects/Item';
 import { Level } from '../../objects/Level';
 import { Events } from '../Events';
-import { GameObject } from '../GameObject';
+import type { GameObject } from '../GameObject';
 import { LevelTransition } from '../LevelTransition';
 import { Resources } from '../Resources';
 import { Sprite } from '../Sprite';
 import { Vector2 } from '../Vector2';
-import { LevelBuilderConfig, LevelExit } from './levelBuilder.types';
+import type { LevelBuilderConfig, LevelExit, LevelMap, LevelsId } from './levelBuilder.types';
 import { GRASS1_LEVEL } from './levels/grass1Level';
+import { PURPLE_LEVEL } from './levels/purpleLevel';
+import { TEST_TILES_LEVEL } from './levels/testTilesLevel';
 import { WORLD_TILES_FRAME_MAP } from './tilesFramesMap';
 
-// TODO: create a LevelsMapper Record with level id and LevelMap object. Get levels dynamically from json and validate them with zod
+const LEVELS: Record<LevelsId, LevelMap> = {
+  grass1Level: GRASS1_LEVEL,
+  purpleLevel: PURPLE_LEVEL,
+  testTilesLevel: TEST_TILES_LEVEL,
+};
+
 export class LevelBuilder extends Level {
   exits: Record<string, LevelExit> = {};
 
   constructor(config: LevelBuilderConfig) {
-    // TODO: config.id is used to select level from LevelsMapper
-    const { id, background, heroDefaultPosition, tiles, walls, exits, gameObjects } = GRASS1_LEVEL;
+    // TODO: uncomment when levels are defined fom a json
+    // const levelMap = LevelsMapper.getLevel(config.id);
+    // if (!levelMap) {
+    //   throw new Error(`Level "${config.id}" not found in LevelsMapper`);
+    // }
+
+    const { id, background, heroDefaultPosition, tiles, walls, exits, gameObjects } = LEVELS[config.id];
 
     super({ id });
 
@@ -126,20 +139,20 @@ export class LevelBuilder extends Level {
 
   override ready(): void {
     Events.on<ExitData>(HERO_EXITS, this, ({ id }) => {
-      const { newLevel, heroNewPosition } = this.exits[id];
+      const { newLevelId, heroNewPosition } = this.exits[id];
+
+      // TODO: uncomment when levels are defined fom a json
+      // if (!LevelsMapper.hasLevel(newLevelId)) {
+      //   throw new Error(`Level "${newLevelId}" not found in LevelsMapper`);
+      // }
 
       LevelTransition.init(() => {
         Events.emit(
           CHANGE_LEVEL,
-          // TODO: newLevel should be a string representing the new level id from json or the class name of the new level
-          typeof newLevel === 'string'
-            ? new LevelBuilder({
-                id: newLevel,
-                heroStartPosition: new Vector2(gridCells(heroNewPosition.x), gridCells(heroNewPosition.y)),
-              })
-            : new newLevel({
-                heroStartPosition: new Vector2(gridCells(3), gridCells(6)),
-              }),
+          new LevelBuilder({
+            id: newLevelId,
+            heroStartPosition: new Vector2(gridCells(heroNewPosition.x), gridCells(heroNewPosition.y)),
+          }),
         );
       });
     });
