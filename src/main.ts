606497e2fd35c46edf4101861037e40e2bc2b437
Alberto Paolucci <apaolucci@softstrategy.it>
2026-05-28 15:36:53 +0200

    refined entry point wip

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,46 @@ import { LevelBuilder } from './lib/LevelBuilder';
 import { Main } from './objects/Main';
 import './style.css';
 
-// Grabbing the canvas to draw to
-const canvas = document.querySelector<HTMLCanvasElement>('#game-canvas')!;
-const ctx = canvas.getContext('2d')!;
-
 /**
  * Initialize the game: load levels, set up the scene, and start the game loop
  */
-// async function initializeGame(): Promise<void> {
+// async function initializeGame(config: Partial<{ containerId: string }>): Promise<void> {
 //   // Load all levels from JSON before starting the game
+//   await Resources.loadResources();
 //   await LevelsMapper.loadLevels();
+//   const gameContainer = document.querySelector<HTMLDivElement>(config.containerId ?? '#game-container');
 // }
 
 // TODO: add logic below inside initializeGame() function
+// Grabbing the container to create canvas inside
+const gameContainer = document.querySelector<HTMLDivElement>('#game-container');
+
+if (!gameContainer) {
+  throw new Error('Game container is not defined');
+}
+
+gameContainer.style.position = 'relative';
+gameContainer.style.margin = '0 auto';
+// gameContainer.style.width = '640px';
+// gameContainer.style.height = '360px';
+
+// Creating the canvas to draw to
+const canvas = document.createElement('canvas');
+canvas.setAttribute('id', 'game-canvas');
+canvas.setAttribute('width', '320');
+canvas.setAttribute('height', '180');
+canvas.style.width = '100%';
+canvas.style.backgroundColor = '#333';
+canvas.style.imageRendering = 'pixelated';
+
+gameContainer.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+
+if (!ctx) {
+  throw new Error('Canvas context is not defined');
+}
+
 // Establish the root scene
 const mainScene = new Main();
 mainScene.setLevel(
