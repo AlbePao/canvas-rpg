59365398fb9bebd12bdc607e89dc280b3064d7ae
Alberto Paolucci <apaolucci@softstrategy.it>
2026-05-31 18:50:22 +0200

    fix npc animations

--- src/objects/Npc/npcAnimations.ts
+++ src/objects/Npc/npcAnimations.ts
@@ -18,15 +18,15 @@ const makeWalkingFrame = (rootFrame = 0): AnimationConfig => ({
       frame: rootFrame,
     },
     {
-      time: 200,
+      time: 100,
       frame: rootFrame + 1,
     },
     {
-      time: 400,
+      time: 200,
       frame: rootFrame + 2,
     },
     {
-      time: 600,
+      time: 300,
       frame: rootFrame + 3,
     },
   ],
@@ -37,7 +37,7 @@ export const NPC_STAND_LEFT = makeStandingFrame(4);
 export const NPC_STAND_RIGHT = makeStandingFrame(8);
 export const NPC_STAND_UP = makeStandingFrame(12);
 
-export const NPC_WALK_DOWN = makeWalkingFrame(1);
-export const NPC_WALK_LEFT = makeWalkingFrame(5);
-export const NPC_WALK_RIGHT = makeWalkingFrame(9);
-export const NPC_WALK_UP = makeWalkingFrame(13);
+export const NPC_WALK_DOWN = makeWalkingFrame(0);
+export const NPC_WALK_LEFT = makeWalkingFrame(4);
+export const NPC_WALK_RIGHT = makeWalkingFrame(8);
+export const NPC_WALK_UP = makeWalkingFrame(12);
