d63bfd9c193d309ebcbc7957a83e1a0211ba04b4
Alberto Paolucci <apaolucci@softstrategy.it>
2026-06-08 17:28:32 +0200

    level tile behavior wip

--- src/objects/LevelTile/LevelTile.ts
+++ src/objects/LevelTile/LevelTile.ts
@@ -1,10 +1,5 @@
-import { HERO_POSITION } from '../../constants/events';
-import { detectOverlap } from '../../helpers/detectOverlap';
-import { getHeroSiblingObject } from '../../helpers/getHeroSiblingObject';
-import { Events } from '../../lib/Events';
 import { GameObject } from '../../lib/GameObject';
 import { Resources } from '../../lib/Resources';
-import type { Vector2 } from '../../lib/Vector2';
 import { Sprite } from '../Sprite';
 import type { LevelTileConfig, LevelTileHeroBehavior } from './levelTile.types';
 import { getLevelTileFrame, getWaterAnimations } from './levelTile.utils';
@@ -32,27 +27,5 @@ export class LevelTile extends GameObject {
     this.addChild(this.body);
 
     this.heroBehavior = heroBehavior;
-  }
-
-  override ready(): void {
-    if (!this.heroBehavior) {
-      return;
-    }
-
-    Events.on<Vector2>(HERO_POSITION, this, (position) => {
-      if (detectOverlap(position, this.position)) {
-        const hero = getHeroSiblingObject(this.parent);
-        if (hero && this.heroBehavior) {
-          // const { heroBehavior, moveDirection } = this.behavior;
-          // if (heroBehavior === 'jump') {
-          //   // hero.jump(moveDirection);
-          // } else if (heroBehavior === 'move') {
-          //   // hero.move(moveDirection);
-          // } else if (heroBehavior === 'spin') {
-          //   // hero.spin(moveDirection);
-          // }
-        }
-      }
-    });
   }
 }
--- src/objects/LevelTile/levelTile.types.ts
+++ src/objects/LevelTile/levelTile.types.ts
@@ -1,20 +1,24 @@
 import type { LevelTileName } from '../../lib/LevelBuilder/tileset.types';
 import type { Vector2 } from '../../lib/Vector2';
+import type { Directions } from '../../types/directions';
 
-// TODO: extract related logic to set hero behavior programmatically
-// TODO: hero can jump over the ledge, like in pokemon
-// TODO: behavior directions are IN_PLACE, FORWARDS (facing direction + 1), BACKWARDS (facing direction - 1)
-export interface LevelTileHeroBehavior {
-  behavior: 'jump' | 'move' | 'spin';
-  direction: 'IN_PLACE' | 'FORWARDS' | 'BACKWARDS';
-  speed?: number;
-}
+export type LevelTileHeroBehaviorDirection = Directions | 'facingDirection';
+
+export type LevelTileHeroBehavior =
+  | {
+      behavior: 'jump';
+      direction?: never;
+      speed?: never;
+    }
+  | {
+      behavior: 'move' | 'spin' | 'slide';
+      direction: LevelTileHeroBehaviorDirection;
+      speed?: number;
+    };
 
 export interface LevelTileConfig {
   id: string;
   tileName: LevelTileName;
   position: Vector2;
-  reflectUpperCell?: boolean;
-  reflectChildren?: boolean;
   heroBehavior?: LevelTileHeroBehavior;
 }
