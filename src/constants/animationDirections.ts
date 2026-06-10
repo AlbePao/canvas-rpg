5174932f174ab2cd9ba9f7ea16c957a3bd3b253c
Alberto Paolucci <apaolucci@softstrategy.it>
2026-06-10 15:42:11 +0200

    animation types

--- src/constants/animationDirections.ts
+++ src/constants/animationDirections.ts
@@ -1,7 +1,7 @@
-import type { AnimationFrame } from '../lib/Animations';
+import type { StandingFrame } from '../lib/Animations';
 import type { Directions } from '../types/directions';
 
-export const STANDING_DIRECTIONS: Record<Directions, AnimationFrame> = {
+export const STANDING_DIRECTIONS: Readonly<Record<Directions, StandingFrame>> = {
   DOWN: 'standDown',
   UP: 'standUp',
   LEFT: 'standLeft',
--- src/lib/Animations/animations.types.ts
+++ src/lib/Animations/animations.types.ts
@@ -3,14 +3,8 @@ import type { LevelWaterTileSet } from '../LevelBuilder/tileset.types';
 
 export type AnimationPattern = Partial<Record<AnimationFrame, FrameIndexPattern>>;
 
-export type AnimationFrame =
-  | 'standDown'
-  | 'standLeft'
-  | 'standRight'
-  | 'standUp'
-  | 'walkDown'
-  | 'walkLeft'
-  | 'walkRight'
-  | 'walkUp'
-  | 'pickUpDown'
-  | LevelWaterTileSet;
+export type StandingFrame = 'standDown' | 'standLeft' | 'standRight' | 'standUp';
+
+export type WalkingFrame = 'walkDown' | 'walkLeft' | 'walkRight' | 'walkUp';
+
+export type AnimationFrame = StandingFrame | WalkingFrame | 'pickUpDown' | LevelWaterTileSet;
