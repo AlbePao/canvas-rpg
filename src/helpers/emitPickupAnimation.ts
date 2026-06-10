0c2072f38873cd12349484992b270461b320c594
Alberto Paolucci <apaolucci@softstrategy.it>
2026-06-10 17:29:15 +0200

    fix imports

--- src/helpers/emitPickupAnimation.ts
+++ src/helpers/emitPickupAnimation.ts
@@ -1,6 +1,7 @@
 import { HERO_PICKS_UP_ITEM } from '../constants/events';
 import { Events } from '../lib/Events';
-import { ITEMS_SPRITE_FRAME, type CollectibleItemData, type ItemKey } from '../objects/Item';
+import type { CollectibleItemData, ItemKey } from '../objects/Item';
+import { ITEMS_SPRITE_FRAME } from '../objects/Item/item.constants';
 
 export function emitPickupAnimation(itemKey: ItemKey, shouldSkipPickupAnimation = false): void {
   Events.emit<CollectibleItemData>(HERO_PICKS_UP_ITEM, {
--- src/objects/Item/Item.ts
+++ src/objects/Item/Item.ts
@@ -1,8 +1,8 @@
 import { createItemSprite } from '../../helpers/createItemSprite';
 import { GameObject } from '../../lib/GameObject';
 import { Vector2 } from '../../lib/Vector2';
+import { ITEMS_SPRITE_FRAME } from './item.constants';
 import type { ItemConfig, ItemData } from './item.types';
-import { ITEMS_SPRITE_FRAME } from './item.types';
 
 export class Item extends GameObject {
   data: ItemData;
