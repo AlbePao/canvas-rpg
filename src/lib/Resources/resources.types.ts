c31eef69b68f534aac2c7335fe92cb887d4517bd
Alberto Paolucci <apaolucci@softstrategy.it>
2026-06-30 18:55:31 +0200

    updated bg resources keys

--- src/lib/Resources/resources.types.ts
+++ src/lib/Resources/resources.types.ts
@@ -5,9 +5,11 @@ export interface Resource {
 
 export type AssetsToLoad = Readonly<Record<ResourceKey, string>>;
 
-export type NpcKey = 'ow1' | 'ow2' | 'ow3' | 'ow4' | 'ow5' | 'ow6' | 'ow7' | 'ow8' | 'ow9' | 'ow10';
+export const NPC_KEYS = ['ow1', 'ow2', 'ow3', 'ow4', 'ow5', 'ow6', 'ow7', 'ow8', 'ow9', 'ow10'] as const;
+export type NpcKey = (typeof NPC_KEYS)[number];
 
-export type BgKey = 'bgCave' | 'bgSky' | 'bgVolcano';
+export const WORLD_BACKGROUNDS = ['bgBeach', 'bgSnow', 'bgWoods'] as const;
+export type BgKey = (typeof WORLD_BACKGROUNDS)[number];
 
 export type ResourceKey =
   | 'chest'
