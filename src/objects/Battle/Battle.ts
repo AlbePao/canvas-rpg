4d323a0583d999fd2982db824a7573ac468756ce
Alberto Paolucci <apaolucci@softstrategy.it>
2026-07-14 17:48:55 +0200

    battle class wip

--- src/objects/Battle/Battle.ts
+++ src/objects/Battle/Battle.ts
@@ -1,9 +1,48 @@
+import { Events } from '../../lib/Events';
+import { Game } from '../../lib/Game';
 import { GameObject } from '../../lib/GameObject';
+import { Resources } from '../../lib/Resources';
+import { Vector2 } from '../../lib/Vector2';
+import type { InteractiveObject } from '../InteractiveObject';
+import { Sprite } from '../Sprite';
+import { BATTLE_END } from './battle.constants';
+import type { BattleConfig } from './battle.types';
 
 export class Battle extends GameObject {
-  constructor() {
+  background: Sprite;
+  starter?: InteractiveObject;
+
+  constructor(config: BattleConfig, starter?: InteractiveObject) {
     super({
       id: 'battle',
     });
+
+    const { background } = config;
+    const { canvasWidth, canvasHeight } = Game.containerSizes;
+
+    this.drawLayer = 'HUD';
+
+    this.background = new Sprite({
+      id: 'battle-background',
+      resource: Resources.images[background],
+      frameSize: new Vector2(canvasWidth, canvasHeight),
+    });
+
+    this.starter = starter;
+  }
+
+  override step(): void {
+    const {
+      input: { getActionJustPressed },
+    } = Game;
+
+    if (getActionJustPressed('KeyQ')) {
+      this.destroy();
+      Events.emit(BATTLE_END);
+    }
+  }
+
+  override drawImage(ctx: CanvasRenderingContext2D, drawPosX: number, drawPosY: number): void {
+    this.background.draw(ctx, drawPosX, drawPosY);
   }
 }
--- src/objects/Battle/battle.types.ts
+++ src/objects/Battle/battle.types.ts
@@ -3,11 +3,20 @@ import type { ItemKey } from '../Item';
 
 export interface BattleConfig {
   background: BgKey;
-  addsFlag: string; // Adds a flag to the story flags when the battle is won
+  addsFlag?: string; // Adds a flag to the story flags when the battle is won
+  // team: BattleTeamMemberConfig;
   winData: {
     text: string[];
     money: number;
     itemKeys: ItemKey[];
     experience: number;
   };
+}
+
+export interface BattleTeamMemberConfig {
+  name: string;
+  level: number;
+  health: number;
+  mana: number;
+  // Add other stats as needed
 }
