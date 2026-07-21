ee216e6b7a510e9fe2f08de4637505bc647a3e5d
Alberto Paolucci <apaolucci@softstrategy.it>
2026-07-21 11:24:05 +0200

    refined animation types

--- src/lib/Animations/animations.types.ts
+++ src/lib/Animations/animations.types.ts
@@ -2,8 +2,13 @@ import type { FrameIndexPattern } from '../FrameIndexPattern';
 
 export type AnimationPattern = Partial<Record<AnimationFrame, FrameIndexPattern>>;
 
-export type StandingFrame = 'standDown' | 'standLeft' | 'standRight' | 'standUp';
+export const ANIMATION_STANDING_FRAMES = ['standDown', 'standLeft', 'standRight', 'standUp'] as const;
+export type StandingFrame = (typeof ANIMATION_STANDING_FRAMES)[number];
 
-export type WalkingFrame = 'walkDown' | 'walkLeft' | 'walkRight' | 'walkUp';
+export const ANIMATION_WALKING_FRAMES = ['walkDown', 'walkLeft', 'walkRight', 'walkUp'] as const;
+export type WalkingFrame = (typeof ANIMATION_WALKING_FRAMES)[number];
 
-export type AnimationFrame = StandingFrame | WalkingFrame | 'collectDown';
+export const ANIMATION_COLLECT_FRAMES = ['collectDown'] as const;
+export type CollectFrame = (typeof ANIMATION_COLLECT_FRAMES)[number];
+
+export type AnimationFrame = StandingFrame | WalkingFrame | CollectFrame;
