8e435b07f034954727aa4f0b466710cc8e162e5f
Alberto Paolucci <apaolucci@softstrategy.it>
2026-07-23 18:20:16 +0200

    MenuScreen backdrop size

--- src/lib/MenuScreen/MenuScreen.ts
+++ src/lib/MenuScreen/MenuScreen.ts
@@ -1,14 +1,14 @@
 import { ArrowIndicator } from '../../objects/ArrowIndicator';
 import { BoxBackdrop } from '../../objects/BoxBackdrop';
-import { Game } from '../Game';
+import { fromGridSize, Game } from '../Game';
 import { GameObject } from '../GameObject';
 import type { MenuScreenConfig } from './menuScreen.types';
 
 export class MenuScreen extends GameObject {
   protected readonly backdrop = new BoxBackdrop({
     id: `${this.id}-inventory-screen-backdrop`,
-    width: Game.containerSizes.canvasWidth,
-    height: Game.containerSizes.canvasHeight,
+    width: fromGridSize(Game.containerSizes.canvasWidth),
+    height: fromGridSize(Game.containerSizes.canvasHeight),
   });
   protected readonly indicator = new ArrowIndicator({
     id: `${this.id}-arrow-indicator`,
